116a1847f30eadb51190ae9bde0a541dfb17676f
Isaque Silveira <73762427+isaque0007@users.noreply.github.com>
2022-05-23 17:42:45 -0300

    Bugfix commit

--- src/main/java/com/security/WebSecurityConfig.java
+++ src/main/java/com/security/WebSecurityConfig.java
@@ -53,7 +53,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
