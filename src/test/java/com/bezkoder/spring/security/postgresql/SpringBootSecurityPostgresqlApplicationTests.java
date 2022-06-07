7c5fb8d9257578d1e0ed8c269b2fe193da9f40c6
Tiago Alcantara <tiago.barros@estudante.iftm.edu.br>
2022-06-06 21:01:51 -0300

    Criação testes

--- src/test/java/com/bezkoder/spring/security/postgresql/SpringBootSecurityPostgresqlApplicationTests.java
+++ src/test/java/com/bezkoder/spring/security/postgresql/SpringBootSecurityPostgresqlApplicationTests.java
@@ -13,4 +13,44 @@ public class SpringBootSecurityPostgresqlApplicationTests {
   public void contextLoads() {
   }
 
+  /*
+  @Test
+  public void testReturnSaveTeacher() {
+	  
+
+	    TeacherDetailsImpl teDetailsImpl;
+	    
+	    Schedules schedules = Schedules.builder()
+	    		.dayWeek("Segunda-feira")
+	    		.initialDate("07")
+	    		.finalDate("11")
+	    		.teachers(null)
+	    		.build();
+	    
+	    Discipline discipline = Discipline.builder()
+	    		.discipline("Teste")
+	    		.flagStandard(1)
+	    		.teachers(null)
+	    		.build();
+	    
+	    List<Schedules> schedulesList = new ArrayList<Schedules>();	    
+	    schedulesList.add(schedules);
+	    
+	    List<Discipline> disciplineList = new ArrayList<Discipline>();	    
+	    disciplineList.add(discipline);
+	    
+	    TeacherDTO teacher = TeacherDTO.builder()
+				.hourCost("10")
+				.fullName("Testonildo")
+				.cpfOrCnpj("130.408.666.68")
+				.email("testonildo@gmail.com")
+				.whatsApp("7777-7777")
+				.linkPhoto("testonildofoto.com.br")
+				.schedules(schedulesList)
+				.description("Prazer testonildo")
+				.disciplines(disciplineList)
+				.build();
+
+	    assertThrows(teDetailsImpl.save(teacher), Teachers.class) ;
+  }*/
 }
